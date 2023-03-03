adaad8133b269acb65f20d4a1c583d7fd51aace8
Léo Boisvert <leo.boisvert@hotmail.ca>
2023-03-02 20:58:39 -0500

    Add instance generator

---
+++ src/models/sat/scripts/generate_data.sh
@@ -0,0 +1,4 @@
+python3 gen_sr_dimacs.py data/raw/ 10 --min_n 3 --max_n 20 --p_geo 0.2 --p_k_2 0.2
+python3 gen_sr_dimacs.py data/raw/ 50 --min_n 3 --max_n 20 --p_geo 0.8 --p_k_2 0.2
+python3 gen_sr_dimacs.py data/raw/ 90 --min_n 3 --max_n 20 --p_geo 0.8 --p_k_2 0.8
+python3 gen_sr_dimacs.py data/raw/ 150 --min_n 3 --max_n 20 --p_geo 0.2 --p_k_2 0.8
