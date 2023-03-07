Fix test bug -without torch.no_grad the gradients accumulate and the model ends up exploding

src/models/sat/train.py
@@ -10,6 +10,8 @@ from torch_geometric.loader import DataLoader
 from torch.nn import BCELoss
 import torch.nn.functional as F
 import matplotlib.pyplot as plt
+import multiprocessing
+multiprocessing.set_start_method('spawn', force=True)
 
 
 def train_one_epoch(model, optimizer, criterion, train_loader):
@@ -18,6 +20,7 @@ def train_one_epoch(model, optimizer, criterion, train_loader):
     total_examples = 0
     total_loss = 0
     for data in train_loader:
+        data = data.to(device="cuda:0")
         optimizer.zero_grad()
         out = model(data.x_dict, data.edge_index_dict, data.batch_dict)
         loss = criterion(out, data["variable"].y)
@@ -36,12 +39,14 @@ def test_model(model, loader, criterion):
     total_loss = 0
     
     for data in loader:  # Iterate in batches over the training/test dataset.
-        out = model(data.x_dict, data.edge_index_dict, data.batch_dict)  
-        loss = criterion(out, data["variable"].y)
-        right_classification += sum(out.argmax(dim=1)
-                                    == data["variable"].y.argmax(dim=1)).item()
-        total_examples += len(data)
-        total_loss += loss.item()
+        data = data.to(device="cuda:0")
+        with torch.no_grad():
+            out = model(data.x_dict, data.edge_index_dict, data.batch_dict)  
+            loss = criterion(out, data["variable"].y)
+            right_classification += sum(out.argmax(dim=1)
+                                        == data["variable"].y.argmax(dim=1)).item()
+            total_examples += len(data)
+            total_loss += loss.item()
 
     return right_classification/total_examples, float(total_loss/total_examples)
 
@@ -85,16 +90,17 @@ def plot_and_save(test_losses, train_losses, test_accs, train_accs, plot_name):
 if __name__ == "__main__":
     test_path = r"C:\Users\leobo\Desktop\École\Poly\Recherche\Generic-Graph-Representation\Graph-Representation\src\models\sat\data"
     dataset = SatDataset(root=test_path)
-    train_dataset = dataset[100:]
-    test_dataset = dataset[:100]
+    train_dataset = dataset[:5000]
+    test_dataset = dataset[5000:5500]
 
-    train_loader = DataLoader(train_dataset, batch_size=64, shuffle=False)
-    test_loader = DataLoader(test_dataset, batch_size=128, shuffle=False)
+    train_loader = DataLoader(train_dataset, batch_size=128, shuffle=False, num_workers=4)
+    test_loader = DataLoader(test_dataset, batch_size=128,
+                             shuffle=False, num_workers=4)
     criterion = torch.nn.BCELoss()
 
-    hidden_units = [64, 128, 256]
-    learning_rates = [0.0005, 0.001, 0.005, 0.1]
-    num_layers = [3, 5, 7]
+    hidden_units = [128, 256]
+    learning_rates = [0.001, 0.005, 0.1]
+    num_layers = [5, 7]
 
     for num_hidden_units in hidden_units:
         for lr in learning_rates: