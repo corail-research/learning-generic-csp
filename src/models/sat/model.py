7a902bfcf9c2a197ef3f6cc04058cfc7cf0da944
Léo Boisvert <leo.boisvert@hotmail.ca>
2023-03-27 17:38:49 -0400

    Add new model combining HGT with GRU

--- src/models/sat/model.py
+++ src/models/sat/model.py
@@ -1,7 +1,7 @@
 import torch
 from torch.nn import Linear
+from torch.nn import GRU
 import torch.nn.functional as F
-from torch_geometric.datasets import OGB_MAG
 from torch_geometric.nn import SAGEConv, GATConv
 from torch_geometric.nn import MeanAggregation
 from torch_geometric.nn import HeteroConv, HGTConv
@@ -89,6 +89,51 @@ class HGT(torch.nn.Module):
 
         return x
     
+class GatedUpdate(torch.nn.Module):
+    def __init__(self, hidden_channels, out_channels, num_heads, num_layers, data, dropout_prob=0):
+        super().__init__()
+
+        self.lin_dict = torch.nn.ModuleDict()
+        for node_type in data.node_types:
+            num_in_features = data[node_type]['x'].size(1)
+            self.lin_dict[node_type] = Linear(num_in_features, hidden_channels)
+
+        self.convs = torch.nn.ModuleList()
+        self.gru_layers = torch.nn.ModuleList()
+        for _ in range(num_layers):
+            conv = HGTConv(hidden_channels, hidden_channels, data.metadata(), num_heads, group='sum')
+            self.convs.append(conv)
+            self.gru_layers.append(GRU(hidden_channels, hidden_channels, batch_first=True))
+
+        self.lin = Linear(hidden_channels, out_channels)
+        self.out_layer = torch.nn.Softmax(dim=1)
+        self.dropout_prob = dropout_prob
+        self.dropout = Dropout(dropout_prob)
+
+    def forward(self, x_dict, edge_index_dict, batch_dict, filename=None):
+        for node_type, x in x_dict.items():
+            x_dict[node_type] = self.lin_dict[node_type](x).relu_()
+        
+        if self.dropout_prob:
+            for conv, gru in zip(self.convs, self.gru_layers):
+                x_dict = conv(x_dict, edge_index_dict)
+                x_dict = {key: self.dropout(value) for key, value in x_dict.items()}
+                x_dict = {key: gru(value.unsqueeze(1))[0].squeeze(1) for key, value in x_dict.items()}
+        else:
+            for conv, gru in zip(self.convs, self.gru_layers):
+                x_dict = conv(x_dict, edge_index_dict)
+                x_dict = {key: gru(value.unsqueeze(1))[0].squeeze(1) for key, value in x_dict.items()}
+
+        diffs = batch_dict["constraint"].diff()
+        diffs[0] = 1
+        indices = (diffs == 1).nonzero(as_tuple=True)
+        main_constraint = x_dict["constraint"][indices]
+        x = self.lin(main_constraint)
+        x = self.out_layer(x)
+
+        return x
+
+
 
 class HGTMeta(torch.nn.Module):
     def __init__(self, hidden_channels, out_channels, num_heads, num_layers, data, dropout_prob=0):
