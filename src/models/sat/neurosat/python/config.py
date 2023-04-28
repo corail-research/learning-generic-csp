54445c3710c8145bbca33a3925cf2f445a2779fb
Léo Boisvert <leo.boisvert@hotmail.ca>
2023-04-28 15:58:40 -0400

    Simplify training loop

--- src/models/sat/neurosat/python/config.py
+++ src/models/sat/neurosat/python/config.py
@@ -1,11 +1,12 @@
 import random
 
 def generate_grid_search_parameters(d, lr_start, dropout, n_msg_layers):
+    cfg = []
     for dim in d:
         for lr in lr_start:
             for drop in dropout:
                 for n_layers in n_msg_layers:
-                    yield {
+                    cfg.append( {
                             'd': dim,  # Dimension of variable and clause embeddings
                             'n_rounds': 26,  # Number of rounds of message passing
                             'lr_decay_type': "exp", # Type of learning rate decay
@@ -24,38 +25,10 @@ def generate_grid_search_parameters(d, lr_start, dropout, n_msg_layers):
                             'torch_seed': 0,  # Random seed for torch
                             'np_seed': 0, # Random seed for numpy
                             'dropout': drop # LSTM Dropout
-                        }
+                        })
 
 d = [128, 256]
 lr_start = [0.0001, 0.001]
 dropout = [0.1, 0.2]
 n_msg_layers = [3, 4]
 CONFIGS = generate_grid_search_parameters(d, lr_start, dropout, n_msg_layers)
-
-# {
-#             'd': 128,  # Dimension of variable and clause embeddings
-#             'n_rounds': 26,  # Number of rounds of message passing
-#             'lr_decay_type': "exp", # Type of learning rate decay
-#             'lr_start': 0.0001,  # Learning rate start
-#             'lr_end': 0.0001,  # Learning rate end
-#             'lr_decay': 0.99,  # Learning rate decay
-#             'lr_decay_steps': 5,  # Learning rate steps decay
-#             'lr_power': 0.5,  # Learning rate decay power
-#             'l2_weight': 0.000000001,  # L2 regularization weight
-#             'clip_val': 0.5,  # Clipping norm
-#             'lstm_transfer_fn': "relu",  # LSTM transfer function
-#             'vote_transfer_fn': "relu",  # MLP transfer function
-#             'final_reducer': "mean", # Reducer for literal votes
-#             'n_msg_layers': 3,  # Number of layers in message MLPs
-#             'n_vote_layers': 3,  # Number of layers in vote MLPs
-#             'torch_seed': 0,  # Random seed for torch
-#             'np_seed': 0, # Random seed for numpy
-#             'dropout': 0.1 # LSTM Dropout
-#         }
-# if __name__ == "__main__":
-#     d = [128, 256]
-#     lr_start = [0.0001, 0.001]
-#     dropout = [0.1, 0.2]
-#     n_msg_layers = [3, 4]
-#     for params in generate_grid_search_parameters(d, lr_start, dropout, n_msg_layers):
-#         print(params)
--- src/models/sat/neurosat/python/main.py
+++ src/models/sat/neurosat/python/main.py
@@ -1,9 +1,13 @@
-import argparse
-import os
+import random
 import subprocess
-import wandb
+import sys
+import os
+import argparse
 from parse import parse
+import wandb
 from config import CONFIGS
+from options import add_neurosat_options
+from neurosat import NeuroSAT
 
 
 parser = argparse.ArgumentParser()
@@ -12,16 +16,20 @@ parser.add_argument('--test', action=argparse.BooleanOptionalAction, dest="test"
 
 parser.add_argument('--train_dir', action='store', dest='train_dir', type=str, default='data/train/sr5')
 parser.add_argument('--test_dir', action='store', dest='test_dir', type=str, default='data/test/sr5')
-parser.add_argument('--n_epochs', action='store', dest='n_epochs', type=int, default=40)
+parser.add_argument('--n_epochs', action='store', dest='n_epochs', type=int, default=1)
 parser.add_argument('--run_id', action='store', dest='run_id', type=int, default=0)
 parser.add_argument('--restore_epoch', action='store', dest='restore_epoch', type=int, default=-1)
+parser.add_argument('--restore_id', action='store', dest='restore_id', type=int, default=None)
 parser.add_argument('--n_rounds', action='store', dest='n_rounds', type=int, default=16)
+parser.add_argument('--n_saves_to_keep', action='store', dest='n_saves_to_keep', type=int, default=4, help='Number of saved models to keep')
+parser.add_argument('--wandb_id', action='store', dest='wandb_id', type=str, default=None, help='Number of saved models to keep')
 
-args = parser.parse_args()
+opts = parser.parse_args()
 
-for config in CONFIGS:
+
+def train_with_config(config):
     n_batches = 0
-    for file in os.listdir(args.train_dir):
+    for file in os.listdir(opts.train_dir):
         parsed = parse("{}npb={}_nb={}.pkl",file)
         max_nodes_per_batch = parsed[1]
         n_batches += int(parsed[2])
@@ -29,18 +37,52 @@ for config in CONFIGS:
     config['Max nodes per batch'] = int(max_nodes_per_batch)
     config['Number of batches'] = n_batches
 
+    setattr(opts, 'commit', subprocess.check_output(['git', 'rev-parse', 'HEAD']).strip())
+    setattr(opts, 'hostname', subprocess.check_output(['hostname']).strip())
+
+    if opts.run_id is None: opts.run_id = random.randrange(sys.maxsize)
+
+    print(opts)
+
+    if not os.path.exists("snapshots/"):
+        os.mkdir("snapshots")
+
+    g = NeuroSAT(opts)
+    
     run = wandb.init(
         project='neurosat',
         entity='seapearl',
         config=config
     )
-    wandb_run_id = run.id
-    print(args)
 
-
-    subprocess.call(
-            f"python3 python/train.py {args.train_dir} --n_epochs {args.n_epochs} --run_id {args.run_id} --wandb_id {wandb_run_id} --test_dir {args.test_dir}",
-            shell=True,
+    for epoch in range(opts.n_epochs):
+        result = g.train_epoch(epoch)
+        (efilename, etrain_cost, etrain_mat, lr, etime) = result
+        
+        wandb.log({
+            "Training Accurracy": etrain_mat.tt+ etrain_mat.ff,
+            "Training Unsat accuracy": etrain_mat.ff / (etrain_mat.ff + etrain_mat.ft),
+            "Training Sat accuracy": etrain_mat.tt / (etrain_mat.tt + etrain_mat.tf),
+            "Training Sat accuracy": etrain_mat.tt / (etrain_mat.tt + etrain_mat.tf),
+            "Training Sat f1 score": etrain_mat.tt / (etrain_mat.tt + 0.5*(etrain_mat.tf + etrain_mat.ft)),
+            "Training Unsat f1 score": etrain_mat.ff / (etrain_mat.ff + 0.5*(etrain_mat.tf + etrain_mat.ft)),
+            "Training Loss": etrain_cost
+            }
         )
 
-wandb.finish()
+        g.test(opts.test_dir)
+
+        print("[Epoch %d] %.4f (%.2f, %.2f, %.2f, %.2f) ---> Accuracy: %.2f [%.2fs]" % (
+            epoch,
+            etrain_cost,
+            etrain_mat.ff,
+            etrain_mat.ft,
+            etrain_mat.tf,
+            etrain_mat.tt,
+            etrain_mat.tt + etrain_mat.ff,
+            etime/10**9))
+    wandb.finish()
+
+if __name__ == '__main__':
+    for config in CONFIGS:
+        train_with_config(config)
