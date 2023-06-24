11ca135f3247c1d470e4bd5187e138fc4bfa93d9
Léo Boisvert <leo.boisvert@hotmail.ca>
2023-06-24 18:34:43 -0400

    training loop update

--- src/models/sat/generic_sat/train.py
+++ src/models/sat/generic_sat/train.py
@@ -84,8 +84,8 @@ def process_model(model, optimizer, criterion, loader, mode='train'):
     report = classification_report(y_true, y_pred, output_dict=True, zero_division=0)
     metrics = {
         f"{mode}/global_acc": report['accuracy'],
-        f"{mode}/acc_class_0": report['0']['precision'],
-        f"{mode}/acc_class_1": report['1']['precision'],
+        f"{mode}/acc_class_0": report['0']['recall'],
+        f"{mode}/acc_class_1": report['1']['recall'],
         f"{mode}/f1_class_0": report['0']['f1-score'],
         f"{mode}/f1_class_1": report['1']['f1-score'],
         f"{mode}/loss": total_loss / len(y_true)
@@ -95,7 +95,7 @@ def process_model(model, optimizer, criterion, loader, mode='train'):
     return report['weighted avg']["precision"], total_loss / len(y_true), metrics
 
 
-def generate_grid_search_parameters(batch_sizes, hidden_units, num_heads, learning_rates, num_layers, dropout, num_epochs):
+def generate_grid_search_parameters(batch_sizes, hidden_units, num_heads, learning_rates, num_layers, dropout, num_epochs, num_lstm_passes):
     for batch_size in batch_sizes:
         for num_hidden_units in hidden_units:
             for heads in num_heads:
@@ -108,10 +108,11 @@ def generate_grid_search_parameters(batch_sizes, hidden_units, num_heads, learni
                             "learning_rate": lr,
                             "num_layers": layers,
                             "dropout": dropout,
-                            "num_epochs": num_epochs
+                            "num_epochs": num_epochs,
+                            "num_lstm_passes": random.choice(num_lstm_passes)
                         }
 
-def generate_random_search_parameters(n, batch_sizes, hidden_units, num_heads, learning_rates, num_layers, dropout, num_epochs):
+def generate_random_search_parameters(n, batch_sizes, hidden_units, num_heads, learning_rates, num_layers, dropout, num_epochs, num_lstm_passes):
     tested_combinations = set()
     count = 0
     
@@ -123,7 +124,8 @@ def generate_random_search_parameters(n, batch_sizes, hidden_units, num_heads, l
             "learning_rate": random.choice(learning_rates),
             "num_layers": random.choice(num_layers),
             "dropout": dropout,
-            "num_epochs": num_epochs
+            "num_epochs": num_epochs,
+            "num_lstm_passes": random.choice(num_lstm_passes)
         }
         
         frozen_params = frozenset(params.items())
@@ -134,22 +136,23 @@ def generate_random_search_parameters(n, batch_sizes, hidden_units, num_heads, l
             yield params
 
 if __name__ == "__main__":
-    search_method = "grid"  # Set to either "grid" or "random"
+    search_method = "random"  # Set to either "grid" or "random"
     test_path = r"../data"
 
     # Hyperparameters for grid search or random search
     batch_sizes = [32]
     hidden_units = [128]
     num_heads = [2, 4]
-    learning_rates = [0.00001, 0.000005]
-    num_layers = [3]
+    learning_rates = [0.00001, 0.0001]
+    num_lstm_passes = [26, 30, 32]
+    num_layers = [2, 3]
     dropout = 0.1
-    num_epochs = 300
+    num_epochs = 400
     device = "cuda:0"
 
     dataset = SatDataset(root=test_path, graph_type="refactored", meta_connected_to_all=False, use_sat_label_as_feature=False)
-    train_dataset = dataset[:100000]
-    test_dataset = dataset[100000:]
+    train_dataset = dataset[:18000]
+    test_dataset = dataset[18000:]
 
     # criterion = torch.nn.CrossEntropyLoss(reduction="sum")
     criterion = torch.nn.BCEWithLogitsLoss(reduction="sum")
@@ -157,10 +160,10 @@ if __name__ == "__main__":
 
     # Generate parameters based on the search method
     if search_method == "grid":
-        search_parameters = generate_grid_search_parameters(batch_sizes, hidden_units, num_heads, learning_rates, num_layers, dropout, num_epochs)
+        search_parameters = generate_grid_search_parameters(batch_sizes, hidden_units, num_heads, learning_rates, num_layers, dropout, num_epochs, num_lstm_passes)
     elif search_method == "random":
         num_random_combinations = 10
-        search_parameters = generate_random_search_parameters(num_random_combinations, batch_sizes, hidden_units, num_heads, learning_rates, num_layers, dropout, num_epochs)
+        search_parameters = generate_random_search_parameters(num_random_combinations, batch_sizes, hidden_units, num_heads, learning_rates, num_layers, dropout, num_epochs, num_lstm_passes)
     else:
         raise ValueError("Invalid search_method. Must be 'grid' or 'random'")
     
@@ -177,11 +180,11 @@ if __name__ == "__main__":
         first_batch = next(first_batch_iter)
         metadata = (list(first_batch.x_dict.keys()), list(first_batch.edge_index_dict.keys()))
 
-        num_hidden_channels = 128
+        num_hidden_channels = params["num_hidden_units"]
         input_size = {key: value.size(1) for key, value in first_batch.x_dict.items()}
         hidden_size = {key: num_hidden_channels for key, value in first_batch.x_dict.items()}
         out_channels = {key: num_hidden_channels for key in first_batch.x_dict.keys()}
-        model = AdaptedNeuroSAT(metadata, input_size, out_channels, hidden_size)
+        model = AdaptedNeuroSAT(metadata, input_size, out_channels, hidden_size, num_passes=params["num_lstm_passes"])
         model = model.cuda()
         optimizer = torch.optim.Adam(model.parameters(),lr=params["learning_rate"],weight_decay=0.0000000001)
     
