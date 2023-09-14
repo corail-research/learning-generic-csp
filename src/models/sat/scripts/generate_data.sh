ea81aa49c8a77e448a909876cf23760a0890da12
Léo Boisvert <leo.boisvert@hotmail.ca>
2023-09-14 09:48:54 -0400

    Update data generation script

--- src/models/sat/scripts/generate_data.sh
+++ src/models/sat/scripts/generate_data.sh
@@ -1,1 +1,1 @@
-python3 gen_sr_dimacs.py data/raw/ 10000 --min_n 3 --max_n 20 --p_geo 0.2 --p_k_2 0.2
+python3 gen_sr_dimacs.py /scratch1/boileo/sat/data/raw 10000 --min_n 3 --max_n 20 --p_geo 0.4 --p_k_2 0.3
