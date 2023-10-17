1ea354888f2cdd03acbd65346674ae6bf14bfb3b
Léo Boisvert <leo.boisvert@hotmail.ca>
2023-10-17 16:52:08 -0400

    Constraint block + group parsing

--- src/parsing/constraints.py
+++ src/parsing/constraints.py
@@ -1,128 +1,14 @@
 import ast
-import re
 from typing import List, Dict
 import xml.etree.ElementTree as ET
-# from intension_utils import *
+from intension_utils import *
+import extension
 from variables import *
+from alldifferent import *
 
 
-def parse_all_different_constraints(raw_alldiff_constraints: List[ET.Element]) -> List[Dict]:
-    """Parses all allDifferent constraints in a given problem
-
-    Args:
-        raw_alldiff_constraints (ET.Element): _description_
-
-    Returns:
-        _type_: _description_
-    """
-    alldiff_constraints = []
-    for constraint in raw_alldiff_constraints:
-        if constraint.find("list") is not None:
-            parsed_constraint = parse_list_and_exceptions(constraint)
-            alldiff_constraints.append(parsed_constraint)
-        elif constraint.find("matrix") is not None:
-            parsed_constraint = parse_matrix(constraint)
-            alldiff_constraints.append(parsed_constraint)
-        else:
-            parsed_constraint = parse_base_alldiff_constraint(constraint)
-            alldiff_constraints.append(parsed_constraint)
-
-    return alldiff_constraints
-
-
-def parse_list_and_exceptions(root: ET.Element):
-    """Parses generic allDifferent constraints; the ones like:
-    <allDifferent id = "c2" >
-        <list > v1 v2 v3 v4 < /list >
-        <list > w1 w2 w3 w4 < /list >
-        <list > z1 z2 z3 z4 < /list >
-        <except > (0, 0, 0, 0) < /except >
-    </allDifferent>
-
-    Args:
-        root (ET.Element): generic allDifferent constraint
-
-    Returns:
-        constraint (Dict): example: {
-            "lists" : [
-                (v1, v2, v3, v4)
-                (w1, w2, w3, w4)
-                (z1, z2, z3, z4)
-            ],
-            "exceptions": ((0,0,0,0))
-        }
-    """
-    lists = []
-    for elem in root.findall("list"):
-        items = elem.text.replace("[]", "").strip().split()
-        lists.append(items)
-
-    except_elem = root.find("except")
-    exceptions = None
-    if except_elem is not None:
-        exceptions = ast.literal_eval(except_elem.text.strip())
-    if exceptions is not None and not isinstance(exceptions, tuple):
-        exceptions = (exceptions)
-    result = {"lists": lists, "exceptions": exceptions}
-
-    return result
-
-
-def parse_matrix(root: ET.Element) -> Dict:
-    """Parses matrix allDifferent constraints; the ones like:
-    < allDifferent >
-        <matrix >
-            (x1, x2, x3, x4, x5)
-            (y1, y2, y3, y4, y5)
-            (z1, z2, z3, z4, z5)
-        </matrix >
-    </allDifferent >
-
-    Args:
-        root (ET.Element): matrix allDifferent constraint
-
-    Returns:
-        constraint (Dict): example: {
-            "matrix" : [
-                (x1, x2, x3, x4, x5)
-                (y1, y2, y3, y4, y5)
-                (z1, z2, z3, z4, z5)
-            ]
-        }
-    """
-    matrix = {}
-    for matrix_element in root.findall("matrix"):
-        matrix["matrix"] = [tuple(row.strip().strip("()").split(
-            ",")) for row in matrix_element.text.strip().split("\n")]
-    return matrix
-        
-
-def parse_base_alldiff_constraint(root: ET.Element) -> Dict:
-    """Parses basic allDifferent constraints; the ones like:
-    <allDifferent>
-       x1 x2 x3 x4 x5
-     </allDifferent>
-
-    Args:
-        root (ET.Element): _description_
-
-    Returns:
-        constraint (Dict): example: {
-            "lists" : [[x, y, z]],
-            "exceptions": [0,0,0]
-
-        }
-        Note: "exceptions" key can be None or list
-    """
-    constraint = {}
-    constraint["lists"] = root.text.strip().split()
-    constraint["exceptions"] = None
-
-    return constraint
-    
-
-def parse_block(block: ET.Element) -> List[Dict]:
-    """Parses a constraint block. Works only for extension constraints. Returns a list of dicts from the parse_extension_constraint
+def parse_constraint_section(block: ET.Element) -> List[Dict]:
+    """Parses the constraint section. Works only for extension constraints. Returns a list of dicts from the parse_extension_constraint
     function
 
     Args:
@@ -131,20 +17,64 @@ def parse_block(block: ET.Element) -> List[Dict]:
     Returns:
         constraints (List[Dict]): parsed extension constraints
     """
-    constraints = []
+    constraints = {}
     for child in block:
         name = child.tag
-        if name == "extension":
-            if child.find("supports") is not None:
-                new_constraint = parse_extension_constraint(child)
+        if name == "block":
+            constraints.update(parse_constraint_section(child))
+        
+        elif name == "group":
+            constraint_type, new_constraint = parse_group(child)
+            if constraints.get(constraint_type) is None:
+                constraints[constraint_type] = [new_constraint]
             else:
-                new_constraint = parse_negative_extension_constraint(child)
-        constraints.append(new_constraint)
+                constraints[constraint_type].append(new_constraint)
+
+        elif name == "extension":
+            constraint_type, new_constraint = parse_extension_constraint(child)
+            if constraints.get(constraint_type) is None:
+                constraints[constraint_type] = [new_constraint]
+            else:
+                constraints[constraint_type].append(new_constraint)
+
+        elif name == "intension":
+            constraint_type, new_constraint = parse_extension_constraint(child)
+            if constraints.get(constraint_type) is None:
+                constraints[constraint_type] = [new_constraint]
+            else:
+                constraints[constraint_type].append(new_constraint)
+        
+        elif name == "alldifferent":
+            constraint_type, new_constraint = parse_all_different_constraints(child)
+            if constraints.get(constraint_type) is None:
+                constraints[constraint_type] = [new_constraint]
+            else:
+                constraints[constraint_type].append(new_constraint)
+        
+        elif name == "element":
+            pass
+
 
     return constraints
 
+def parse_group(group: ET.Element) -> List[Dict]:
+    """Parse a group block: Identifies the type of constraint and parses it accordingly
+    """
+    if group.find("extension"):
+        constraint_type, new_constraint = parse_extension_group(group)
+    elif group.find("intension"):
+        constraint_type, new_constraint = parse_intension_group(group)
+    elif group.find("element"):
+        pass
+    elif group.find("allDifferent"):
+        pass
+    elif group.find("sum"):
+        pass
+    
+    return constraint_type, new_constraint
 
-def parse_extension_constraint(constraint: ET.Element) -> Dict:
+
+def parse_extension_group(constraint: ET.Element) -> Dict:
     """Parse an individual extension constraint defined with supports; that is values the variables CAN take. 
     Extension constraints defined with exclusion are parsed with function parse_negative_extension_constraint
     Returns a dict like:
@@ -157,69 +87,31 @@ def parse_extension_constraint(constraint: ET.Element) -> Dict:
         parsed_constraint: dict containing the parsed constraint
     """
     parsed_constraint = {}
-    variables = constraint.find("list").text.strip().split()
+
+    variables = []
+    for arg in constraint.findall('args'):
+        variables.append((arg.text.split()[0], arg.text.split()[1]))
     parsed_constraint["variables"] = variables
-    tuples = ast.literal_eval(constraint.find(
-        "supports").text.strip().replace(")", "),"))
-    parsed_constraint["tuples"] = tuples
-
-    return parsed_constraint
-
-
-def parse_negative_extension_constraint(constraint: ET.Element) -> Dict:
-    """Parse an individual extension constraint defined with conflicts; that is values the variables can't take.
-    Returns a dict like:
-    {"variables": [x, y], "tuples": ((1, 2), (2, 3))}, which means variables (x, y) can't take values (1, 2) or (2, 3)
-
-    Args:
-        constraint (ET.Element): element of "extension" ; extension constraint
-    Returns:
-        parsed_constraint: dict containing the parsed constraint
-    """
-    parsed_constraint = {}
-    variables = constraint.find("list").text.strip().split()
-    parsed_constraint["variables"] = variables
-    tuples = ast.literal_eval(constraint.find("conflicts").text.strip().replace(")", "),"))
-    parsed_constraint["tuples"] = tuples
-
-    return parsed_constraint
-
-
-def parse_intension_group(group, base_intension_expression):
-    parsed_intension_constraints = []
-    args = group.findall("args")
-    for raw_arg in args:
-        arg_format = raw_arg.text.strip().split()
-        formatted = replace_placeholders(base_intension_expression, arg_format)
-        parsed = parse_intension_expression(formatted)
-        parsed_intension_constraints.append(parsed)
     
-    return parsed_intension_constraints
+    # Get the support tuples
+    support_tuples = []
+    supports = constraint.find('extension/supports')
+    if supports is not None:
+        supports = supports.text.split()
+        for support in supports:
+            support_tuples.append(ast.literal_eval(support.replace(")", "),")))
+        parsed_constraint["supports"] = support_tuples
+        
+    # Get the conflict tuples
+    conflict_tuples = []
+    conflicts = constraint.find('extension/conflicts')
+    if conflicts is not None:
+        conflicts = conflicts.text.split()
+        for conflict in conflicts:
+            conflict_tuples.extend(ast.literal_eval(conflict.replace(")", "),")))
+        parsed_constraint["conflicts"] = conflict_tuples
 
-def parse_intension_expression(expression):
-    # Split the expression into operator and operands
-    split = re.split(r"\((.*)\)", expression)
-    operator = split[0]
-    if operator not in intension_operators:
-        # If the expression does not have any further parentheses, return it as a string
-        return expression
-    operands_str = split[1]
-    # Recursively parse the operands
-    operands = []
-    start = 0
-    balance = 0
-    for i, char in enumerate(operands_str):
-        if char == "(":
-            balance += 1
-        elif char == ")":
-            balance -= 1
-        elif char == "," and balance == 0:
-            operands.append(parse_intension_expression(operands_str[start:i]))
-            start = i + 1
-    operands.append(parse_intension_expression(operands_str[start:]))
-
-    # Return the IntensionNode for the expression
-    return IntensionNode(operator, operands)
+    return "extension", parsed_constraint
 
 if __name__ == "__main__":
     # TODO: add tests for extension constraints
@@ -264,9 +156,14 @@ if __name__ == "__main__":
     test_ad_constraints = [ET.fromstring(i) for i in test_cases]
     parse_all_different_constraints(test_ad_constraints)
 
-    file_path = r"C:/Users/leobo/Desktop/École/Poly/SeaPearl/instancesXCSP22/MiniCOP/ClockTriplet-20-35_c22.xml..xml"
+    # file_path = r"C:/Users/leobo/Desktop/École/Poly/SeaPearl/instancesXCSP22/MiniCOP/LowAutocorrelation-015_c18.xml..xml"
+    file_path = r"C:\Users\leobo\Desktop\École\Poly\SeaPearl\instancesXCSP22\MiniCSP\Rlfap-ext-scen-11-f12_c18.xml..xml"
     root = ET.parse(file_path)
     constraints = root.findall("constraints")
-    groups = constraints[0].findall("group")
+    # groups = constraints[0].findall("group")
     # base_intension_expression = groups[0].find("intension").text.strip()
-    # parse_intension_group(groups[0], base_intension_expression)
+    # intension_groups = parse_intension_group(groups[0], base_intension_expression)
+    groups = constraints[0].findall("group")
+    # for group in groups:
+    parse_constraint_section(groups)
+    a=1
