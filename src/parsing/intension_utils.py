fbf3347e508fe20f467938923b9e5e5a8d651f98
Léo Boisvert <leo.boisvert@hotmail.ca>
2023-10-19 16:26:54 -0400

    Refactor intension parser

--- src/parsing/intension_utils.py
+++ src/parsing/intension_utils.py
@@ -31,7 +31,7 @@ intension_operators = {
     "or",
 }
 
-def parse_intension_group(group, base_intension_expression):
+def parse_intension_group(group):
     """Parses a group of intension constraints in a given problem
 
     Args:
@@ -41,17 +41,18 @@ def parse_intension_group(group, base_intension_expression):
     Returns:
         List[IntensionNode]: A list of IntensionNode objects representing the parsed intension constraints
     """
+    base_intension_expression = group.find("intension").text.strip()
     parsed_intension_constraints = []
     args = group.findall("args")
     for raw_arg in args:
         arg_format = raw_arg.text.strip().split()
         formatted = replace_placeholders(base_intension_expression, arg_format)
-        parsed = parse_intension_expression(formatted)
+        parsed = parse_intension_constraint(formatted)
         parsed_intension_constraints.append(parsed)
     
-    return parsed_intension_constraints
+    return "intension", parsed_intension_constraints
 
-def parse_intension_expression(expression):
+def parse_intension_constraint(expression):
     """Parses an intension expression in a given problem
 
     Args:
@@ -61,6 +62,8 @@ def parse_intension_expression(expression):
         Union[str, IntensionNode]: Either a string representing the parsed expression, or an IntensionNode object representing the parsed expression
     """
     # Split the expression into operator and operands
+    if type(expression) is not str:
+        expression = expression.text.strip()
     split = re.split(r"\((.*)\)", expression)
     operator = split[0]
     if operator not in intension_operators:
@@ -77,9 +80,9 @@ def parse_intension_expression(expression):
         elif char == ")":
             balance -= 1
         elif char == "," and balance == 0:
-            operands.append(parse_intension_expression(operands_str[start:i]))
+            operands.append(parse_intension_constraint(operands_str[start:i]))
             start = i + 1
-    operands.append(parse_intension_expression(operands_str[start:]))
+    operands.append(parse_intension_constraint(operands_str[start:]))
 
     # Return the IntensionNode for the expression
     return IntensionNode(operator, operands)
