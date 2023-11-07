a5c083df0173322fd8a1897809c726eb0c3b85a7
Léo Boisvert <leo.boisvert@hotmail.ca>
2023-11-07 13:41:37 -0500

    Generic model pipeline

---
+++ src/parsing/generic_model.py
@@ -0,0 +1,171 @@
+from collections import namedtuple
+import torch
+from torch.nn import LSTMCell
+from torch_scatter import scatter_mean
+from typing import Dict
+from models.common.pytorch_models import MLP, LayerNormLSTMCell
+from models.common.lstm_conv import AdaptedNeuroSAT, LSTMConvV1
+
+
+class GenericModel(AdaptedNeuroSAT):
+    def __init__(self,
+                metadata,
+                in_channels:Dict[str, int],
+                out_channels:Dict[str, int],
+                hidden_size:Dict[str, int]=128,
+                num_passes:int=20,
+                device="cpu",
+                flip_inputs:bool=False,
+                layernorm_lstm_cell:bool=True,
+                **kwargs
+            ):
+        """
+        Args:
+            metadata (_type_): metadata for the heterogeneous graph
+            in_channels (Dict[str, int]): Dict mapping node type to the number of input features
+            out_channels (Dict[str, int]): Dict mapping node type to the number of output features
+            hidden_size (Dict[str, int], optional): Dict mapping node types to number of hidden channels. Defaults to 128.
+            num_passes (int): number of LSTM passes
+            device (str): device where computations happen
+            flip_inputs (bool): whether or not to flip the inputs for variable hidden states
+            layernorm_lstm_cell (bool): whether or not to use the layernorm lstm cell
+        """
+        super(AdaptedNeuroSAT, self).__init__()
+        self.in_channels = in_channels
+        self.out_channels = out_channels
+        self.hidden_size = hidden_size
+        self.num_passes = num_passes
+        self.device = device
+        self.projection_layers = torch.nn.ModuleDict()
+        self.projection_division = {}
+        self.vote = MLP(hidden_size["variable"], 2, 1, hidden_size["variable"], device=device)
+        lstm_hidden_sizes = {node_type: hidden_size[node_type] for node_type in metadata[0]}
+        self.lstm_conv_layers = NeuroSatLSTMConv(lstm_hidden_sizes, lstm_hidden_sizes, metadata=metadata, device=device, flip_inputs=flip_inputs, layernorm_lstm_cell=layernorm_lstm_cell, **kwargs)
+        for node_type in metadata[0]:
+            projection_layer = torch.nn.Linear(in_channels[node_type], hidden_size[node_type], bias=False)
+            torch.nn.init.normal_(projection_layer.weight, mean=0.0, std=1)
+            self.projection_layers[node_type] = projection_layer
+            self.projection_division[node_type] = torch.sqrt(torch.tensor(hidden_size[node_type]).float())
+        
+    def forward(self, x_dict, edge_index_dict, batch_dict):
+        with torch.no_grad():
+            for node_type, x in x_dict.items():
+                with torch.no_grad():
+                    embedding_init = self.projection_layers[node_type](x)
+                    x_dict[node_type] = embedding_init / self.projection_division[node_type]
+        for i in range(self.num_passes):
+            if i == 0:
+                previous_hidden_state = {node_type: value for node_type, value in x_dict.items()}
+                previous_cell_state = {node_type: None for node_type, x in x_dict.items()}
+            else:
+                previous_hidden_state = {node_type: out[node_type][0] for node_type in x_dict.keys()}
+                previous_cell_state = {node_type: out[node_type][1] for node_type in x_dict.keys()}
+            out = self.lstm_conv_layers(x_dict, edge_index_dict, previous_hidden_state, previous_cell_state, batch_dict)
+            x_dict = {node_type: out[node_type][1] for node_type in x_dict.keys()}
+        raw_votes = self.vote(x_dict["variable"])
+        votes = scatter_mean(raw_votes, batch_dict["variable"], dim=0)
+
+        return votes
+
+class NeuroSatLSTMConv(LSTMConvV1):
+    """This class is used to perform LSTM Convolution in GNNs. It assumes that the input x_dict 
+    has not yet been passed through an MLP layer. Instead, it concatenates the input features 
+    coming from the neighboring nodes before passing them through the MLP.
+    """
+    def __init__(self, in_channels:Dict, out_channels:Dict, device=None, metadata=None, layernorm_lstm_cell=True, **kwargs):
+        super().__init__(in_channels=in_channels, out_channels=out_channels, device=device, metadata=metadata, **kwargs)
+        self.device = device if device is not None else torch.device('cpu')
+        self.entering_edges_per_node_type = self.get_entering_edge_types_per_node_type(metadata[1], metadata[0])
+        self.input_type_per_node_type = self.get_input_per_node_type(metadata[1], metadata[0])
+        self.lstm_cells = torch.nn.ModuleDict()
+        self.mlp_blocks = torch.nn.ModuleDict()
+        self.lstm_sizes = {}
+        self.flip_inputs = kwargs.get("flip_inputs", False)
+
+        for node_type in metadata[0]:            
+            hidden_size = in_channels[node_type]
+            for edge_type in self.entering_edges_per_node_type[node_type]:
+                src_node_type = edge_type[0]
+                mlp_input_size = in_channels[src_node_type]
+                self.mlp_blocks[str(edge_type)] = MLP(mlp_input_size, 2, hidden_size, hidden_size, device=self.device)
+            lstm_input_size = sum([in_channels[src_node_type] for src_node_type in self.input_type_per_node_type[node_type]])            
+            self.lstm_sizes[node_type] = lstm_input_size
+            
+            if layernorm_lstm_cell:
+                self.lstm_cells[node_type] = LayerNormLSTMCell(lstm_input_size, hidden_size, torch.relu, device=self.device)
+            else:
+                self.lstm_cells[node_type] = LSTMCell(lstm_input_size, hidden_size, device=self.device)
+            
+        
+        self.reset_parameters()
+
+    def forward(self, x_dict, edge_index_dict, previous_hidden_states:Dict, previous_cell_states:Dict, batch_dict: Dict):
+        # Loop over each node type and each edge type
+        sizes = {node_type: x_dict[node_type].size() for node_type in x_dict.keys()}
+        output = {}
+        for node_type in x_dict.keys():
+            inputs = []
+            hidden_state = previous_hidden_states[node_type]
+            for edge_type in self.entering_edges_per_node_type[node_type]:
+                # Perform the message passing for the current node type and edge type
+                source_node_type, _, _ = edge_type
+                x = x_dict[source_node_type]
+                if edge_type == ("variable", "is_negation_of", "variable"):
+                    if self.flip_inputs:
+                        flipped = self.flip(hidden_state, batch_dict)
+                        inputs.append(flipped)
+                    else:
+                        inputs.append(hidden_state)
+                else:
+                    x = self.mlp_blocks[str(edge_type)](x) # before the layer, x is the result of the projection 
+                    edge_index = edge_index_dict[edge_type]
+                    size = (sizes[source_node_type][0], sizes[node_type][0])
+                    agg = self.propagate(edge_index, size=size, x=x, edge_type=edge_type) # Here, we perform the "add" aggregation after the base features are passed through an MLP
+                    # Append the resulting node features to the list of hidden states
+                    inputs.append(agg)
+            # Concatenate the resulting node features for each node type into a single vector
+            h_cat = torch.cat(inputs, dim=1)            
+            if previous_cell_states[node_type] is None:
+                cell_state = torch.zeros_like(hidden_state)
+            else:
+                cell_state = previous_cell_states[node_type]
+            """
+            This part is edge-type-dependent: 
+            for lit -> clause, we perform the update in the following way:
+                - The LSTM input is directly the aggregated output of the mlp (agg = self.propagate ...)
+                - The hidden and cell states are the following:
+                    - The initial LSTM cell state is a zeros tensor of the same size as the hidden state
+                    - The initial LSTM hidden state is the result of the initial projection
+                - For the following steps, they are the following:
+                    - The LSTM cell state is the cell state from the previous step
+                    - The LSTM hidden state is the hidden state from the previous step
+            for clause -> lit, we perform the update in the following way:
+                - The LSTM input is the concatenation of the aggregated output of the mlp and the flipped hidden state from the previous step. 
+                  For the first step, the hidden state is the result of the initial projection.
+                - The hidden and cell states are the following:
+                    - The initial LSTM cell state is a zeros tensor of the same size as the hidden state
+                    - The initial LSTM hidden state is the result of the initial projection
+                - For the following steps, they are the following:
+                    - The LSTM cell state is the cell state from the previous step
+                    - The LSTM hidden state is the hidden state from the previous step
+            """ 
+            h, c = self.lstm_cells[node_type](h_cat, (hidden_state, cell_state))
+            output[node_type] = (h, c)
+
+        return output
+    
+    def flip(self, hidden_state, batch_dict):
+        """Performs the flip operation, as describred in the NeuroSAT paper. This operation replaces -"flips"- the hidden state 
+        of the variable nodes with the hidden state of the negated variable
+        """
+        num_vars_per_instance = torch.bincount(batch_dict["variable"])
+        offset = 0
+        new_index = []
+        for i, num_variables in enumerate(num_vars_per_instance):
+            flipped_indices = torch.arange(offset + num_variables - 1, offset - 1, -1, device=self.device)
+            new_index.append(flipped_indices)
+            offset += num_variables
+        new_index = torch.cat(new_index)
+        flipped = torch.index_select(hidden_state, 0, index=new_index.long())
+
+        return flipped
---
+++ src/train_generic_model.py
@@ -0,0 +1,156 @@
+import socket
+from datetime import datetime
+import wandb
+import torch
+from torch.optim.lr_scheduler import LambdaLR
+import os
+os.environ["CUDA_LAUNCH_BLOCKING"] = "1"
+import cProfile
+import pstats
+# from models.common.lstm_conv import AdaptedNeuroSAT
+from parsing.generic_model import GenericModel
+from parsing.dataset import XCSP3Dataset
+from torch_geometric.loader import DataLoader
+import multiprocessing
+multiprocessing.set_start_method("spawn", force=True)
+
+from models.common.training_utils import train_model
+from models.sat.config import SATExperimentConfig
+from models.common.pytorch_lr_scheduler import  GradualWarmupScheduler
+from models.common.pytorch_samplers import  PairNodeSampler, PairBatchSampler
+
+if __name__ == "__main__":
+    import math
+    search_method = "grid"  # Set to either "grid" or "random"
+    # data_path = r"./src/models/sat/generic/temp_remote_date" # local
+    data_path = r"C:\Users\leobo\Desktop\École\Poly\Recherche\Generic-Graph-Representation\Graph-Representation\src\models\decision_tsp\data" # servercd 
+    # Hyperparameters for grid search or random search
+    batch_sizes = [64]
+    hidden_units = [128, 256]
+    start_learning_rates = [0.00002]
+    num_lstm_passes = [26]
+    num_layers = [3]
+    dropout = [0.1]
+    num_epochs = 400
+    device = "cuda:0"
+    train_ratio = 0.8
+    samples_per_epoch = 200000
+    nodes_per_batch= [12000]
+    use_sampler_loader = False
+    weight_decay = [0.0000000001]
+    num_epochs_lr_warmup = 5
+    num_epochs_lr_decay = 20
+    lr_decay_factor = 0.8
+    generic_representation = True
+    gnn_aggregation = "add"
+    model_save_path = None
+    project_name = "Generic-TSP"
+    # model_save_path = r"./src/models/sat/models/"
+    
+    hostname = socket.gethostname()
+
+    experiment_config = SATExperimentConfig(
+        batch_sizes=batch_sizes,
+        hidden_units=hidden_units,
+        start_learning_rates=start_learning_rates,
+        num_layers=num_layers,
+        dropouts=dropout,
+        num_epochs=num_epochs,
+        num_lstm_passes=num_lstm_passes,
+        device=device,
+        train_ratio=train_ratio,
+        samples_per_epoch=samples_per_epoch,
+        nodes_per_batch=nodes_per_batch,
+        data_path=data_path,
+        use_sampler_loader=use_sampler_loader,
+        weight_decay=weight_decay,
+        num_epochs_lr_warmup=num_epochs_lr_warmup,
+        num_epochs_lr_decay=num_epochs_lr_decay,
+        lr_decay_factor=lr_decay_factor,
+        generic_representation=generic_representation,
+        flip_inputs=False,
+        lr_scheduler_patience=10,
+        lr_scheduler_factor=0.2,
+        layernorm_lstm_cell=True,
+        gnn_aggregation=gnn_aggregation,
+        model_save_path=model_save_path
+    )
+
+    # Generate parameters based on the search method
+    if search_method == "grid":
+        search_parameters = experiment_config.generate_grid_search_parameters()
+    elif search_method == "random":
+        num_random_combinations = 10
+        search_parameters = experiment_config.generate_random_search_parameters(num_random_combinations)
+    else:
+        raise ValueError("Invalid search_method. Must be 'grid' or 'random'")
+    
+    dataset = XCSP3Dataset(root=experiment_config.data_path)
+    train_dataset = dataset[:math.floor(len(dataset) * experiment_config.train_ratio)]
+    test_dataset = dataset[math.floor(len(dataset) * experiment_config.train_ratio):]
+            
+    criterion = torch.nn.BCEWithLogitsLoss(reduction="sum")
+    date = str(datetime.now().date())
+
+    for params in search_parameters:
+        if params.use_sampler_loader:
+            train_sampler = PairNodeSampler(train_dataset, params.nodes_per_batch)
+            train_loader = DataLoader(train_dataset, batch_size=1, sampler=train_sampler, num_workers=0)
+        else:
+            train_sampler = PairBatchSampler(train_dataset, params.batch_size) 
+            train_loader = DataLoader(train_dataset, batch_size=1, sampler=train_sampler, num_workers=0)
+        
+        test_loader = DataLoader(test_dataset, batch_size=min(1024, len(test_dataset)), shuffle=False, num_workers=0)
+        first_batch_iter = iter(test_loader)
+        first_batch = next(first_batch_iter)
+        metadata = (list(first_batch.x_dict.keys()), list(first_batch.edge_index_dict.keys()))
+        num_hidden_channels = params.hidden_units
+        input_size = {key: value.size(1) for key, value in first_batch.x_dict.items()}
+        hidden_size = {key: num_hidden_channels for key, value in first_batch.x_dict.items()}
+        out_channels = {key: num_hidden_channels for key in first_batch.x_dict.keys()}
+        # model_type = "sat_spec"
+        model = GenericModel(
+            metadata,
+            input_size,
+            out_channels,
+            hidden_size,
+            num_passes=params.num_lstm_passes,
+            device=device,
+            flip_inputs=params.flip_inputs,
+            layernorm_lstm_cell=params.layernorm_lstm_cell,
+            aggr=params.gnn_aggregation
+        )
+        # model = AdaptedNeuroSAT(metadata, input_size, out_channels, hidden_size, num_passes=params.num_lstm_passes, device=device)
+        model = model.cuda()
+        optimizer = torch.optim.Adam(model.parameters(),lr=params.start_learning_rate, weight_decay=params.weight_decay)
+        # if type(model) == AdaptedNeuroSAT:
+        group = hostname
+        wandb.init(
+            project=project_name,
+            config=params,
+            group=group
+        )
+        if params.lr_scheduler_patience is not None:
+            lr_scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(optimizer, mode='min', factor=params.lr_scheduler_factor, patience=params.lr_scheduler_patience, verbose=True)
+        else:
+            lr_scheduler = None
+        train_model(
+            model,
+            train_loader,
+            test_loader,
+            optimizer,
+            lr_scheduler,
+            criterion,
+            params.num_epochs,
+            samples_per_epoch=params.samples_per_epoch,
+            clip_value=params.clip_gradient_norm,
+            model_save_path=params.model_save_path,
+            wandb_run_name=wandb.run.name
+        )
+        # profile = cProfile.Profile()
+        # profile.run('train_model(model, train_loader, test_loader, optimizer, lr_scheduler, criterion, params.num_epochs, samples_per_epoch=params.samples_per_epoch, clip_value=0.65)')
+
+        # stats = pstats.Stats(profile)
+        # stats.sort_stats('tottime')
+        # stats.print_stats()
+        wandb.finish() 
