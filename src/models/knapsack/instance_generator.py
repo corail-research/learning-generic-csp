a65999ba1ffcfbea903c6662e17d7729f20f4cbe
Léo Boisvert <leo.boisvert@hotmail.ca>
2023-11-10 14:51:28 -0500

    Add XML output to knapsack instance generator

--- src/models/knapsack/instance_generator.py
+++ src/models/knapsack/instance_generator.py
@@ -16,38 +16,65 @@ def knapsack(weights, values, W, n):
 
     return dp[n][W]
 
-def generate_knapsack_instance(n_items, max_weight, max_value):
+def generate_knapsack_instance(n_items_range, max_weight, max_value):
+    n_items = random.randint(n_items_range[0], n_items_range[1])
     weights = [random.randint(1, max_weight) for _ in range(n_items)]
     values = [random.randint(1, max_value) for _ in range(n_items)]
     W = random.randint(n_items, n_items * max_weight)
-    return weights, values, W
+    return weights, values, W, n_items
 
-def save_instance_to_file(filepath, instance, index):
-    filename = f"instance_{index}.json"
-    filepath = os.path.join(filepath, filename)
-    with open(filename, 'w') as file:
-        json.dump(instance, file, indent=4)
-
-def build_knapsack_dataset(n_instances, n_items, max_weight, max_value, save_path='knapsack_instances'):
+def build_knapsack_dataset(n_instances, n_items_range, max_weight, max_value, save_path='knapsack_instances'):
     # Ensure there's a directory called 'knapsack_instances' to save the instances
     if not os.path.exists(save_path):
         os.makedirs(save_path)
-
-    for i in range(n_instances):
-        weights, values, W = generate_knapsack_instance(n_items, max_weight, max_value)
+    i = 0
+    while i < n_instances:
+        weights, values, W, n_items = generate_knapsack_instance(n_items_range, max_weight, max_value)
         optimal_value = knapsack(weights, values, W, n_items)
-        instance = {
-            "weights": weights,
-            "values": values,
-            "W": W,
-            "optimal_value": optimal_value
-        }
-        save_instance_to_file(save_path, instance, i+1)
+        if optimal_value > 0:
+            instance = {
+                "weights": weights,
+                "values": values,
+                "W": W,
+                "optimal_value": optimal_value
+            }
+            save_instance_to_file(save_path, instance, i + 1)
+            i += 1
+
+def save_instance_to_file(filepath, instance, index):
+    filename = f"instance_{index}.xml"
+    filepath = os.path.join(filepath, filename)
+    with open(filepath, 'w') as file:
+        file.write('<instance format="XCSP3" type="COP">\n')
+        file.write('  <variables>\n')
+        file.write(f'    <array id="x" size="[{len(instance["weights"])}]"> 0..1 </array>\n')
+        file.write('  </variables>\n')
+        file.write('  <constraints>\n')
+        file.write('    <sum id="c1">\n')
+        file.write(f'      <list> {" ".join([f"x[{i}]" for i in range(len(instance["weights"]))])} </list>\n')
+        file.write(f'      <coeffs> {" ".join(map(str, instance["weights"]))} </coeffs>\n')
+        file.write(f'      <condition> (le,{instance["W"]}) </condition>\n')
+        file.write('    </sum>\n')
+        file.write('  </constraints>\n')
+        file.write('  <objectives>\n')
+        file.write('    <minimize type="sum">\n')
+        file.write(f'      <list> {" ".join([f"x[{i}]" for i in range(len(instance["values"]))])} </list>\n')
+        file.write(f'      <coeffs> {" ".join(map(str, instance["values"]))} </coeffs>\n')
+        file.write('    </minimize>\n')
+        file.write(f'    <optimal>{instance["optimal_value"]}</optimal>\n')
+        file.write('  </objectives>\n')
+        file.write('</instance>\n')
+
 
 # Parameters
-N_INSTANCES = 100
-N_ITEMS = 10
+N_INSTANCES = 10000
+N_ITEMS = (20, 40)
 MAX_WEIGHT = 50
 MAX_VALUE = 100
-
+import time
+start = time.time()
 build_knapsack_dataset(N_INSTANCES, N_ITEMS, MAX_WEIGHT, MAX_VALUE)
+end = time.time()
+print(f"Time elapsed: {end-start} seconds")
+#time per instance
+print(f"Time per instance: {(end-start)/N_INSTANCES} seconds")
